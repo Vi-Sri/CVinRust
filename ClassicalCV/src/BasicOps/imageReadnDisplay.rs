4a22361030c1a56c88ed8369018ff25ba5953e92
Vi-Sri <nikola.tesla195@gmail.com>
2021-04-08 14:22:23 +0530

    adding new ops

--- ClassicalCV/src/BasicOps/imageReadnDisplay.rs
+++ ClassicalCV/src/BasicOps/imageReadnDisplay.rs
@@ -1,28 +1,19 @@
 extern crate opencv;
 
 use opencv::{
-    core,
     highgui,
-    imgproc,
-    prelude::*
+    imgcodecs,
 };
 
 fn run() -> opencv::Result<()> {
     let window: &str = "image capture";
-    let image_file: &str  = "/assets/dtp3.mp4";
-    
+    let image = imgcodecs::imread("../../assets/lena.jpg", imgcodecs::IMREAD_COLOR)?;
     highgui::named_window(window, 1)?;
-    
-    
-    loop {
-        let mut frame = core::Mat::default()?;
-        videostream.read(&mut frame)?;
-        if frame.size()?.width > 0 {
-            highgui::imshow(window, &frame)?;
-        }
-        if highgui::wait_key(10)? > 0 {
-            break;
-        }
-    }
-    Ok(())
+    highgui::imshow(window, &image)?;
+	highgui::wait_key(10000)?;
+	Ok(())
+}
+
+fn main() {
+    run().unwrap()
 }
--- ClassicalCV/src/BasicOps/videoReadnDisplay.rs
+++ ClassicalCV/src/BasicOps/videoReadnDisplay.rs
@@ -3,23 +3,20 @@ extern crate opencv;
 use opencv::{
     core,
     highgui,
-    imgproc,
     prelude::*,
     videoio,
 };
 
 fn run() -> opencv::Result<()> {
     let window = "video capture";
-    let video_file  = "/Users/srinivas_v/Work/Projects/CarCounterYOLOv3/videos/dtp3.mp4";
+    let video_file  = "../../assets/dtp3.mp4";
     
     highgui::named_window(window, 1)?;
-    #[cfg(feature = "opencv-4")]
-    let mut cam = videoio::VideoCapture::new_default(0)?;  
     #[cfg(not(feature = "opencv-4"))]
     let mut videostream = videoio::VideoCapture::from_file(video_file, videoio::CAP_ANY)?;
     let opened = videoio::VideoCapture::is_opened(&videostream)?;
     if !opened {
-        panic!("Unable to open default camera!");
+        panic!("Unable to read frame from video or end of video!");
     }
     loop {
         let mut frame = core::Mat::default()?;
